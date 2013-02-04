f01cddb38fc61a8a4178024004f025f42d821f3a
iFadi <fadi_asbih@yahoo.de>
2013-02-04 12:39:45 +0000

    Added a download link for new releases

--- src/view/InputPanel.java
+++ src/view/InputPanel.java
@@ -22,7 +22,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import model.GenerateXML;
 import model.ParseCSV;
 import model.ParseExcel;
-import model.Version;
 import controller.IFile;
 
 /**
@@ -34,29 +33,30 @@ import controller.IFile;
  * @version $Revision$
  * @copyright $Date$
  * 
- * TERMS AND CONDITIONS:
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * TERMS AND CONDITIONS: This program is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  * 
  */
-public class InputPanel extends JPanel implements ActionListener{
+public class InputPanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = -335799796636612645L;
 	private final static String REVISION = "$Rev$";
 	private JButton open;
 	private JButton generate;
 	private JButton exit;
-	private JButton bug;
+	private JButton bugOrDownload;
 	private String filename;
 	private String dir;
 	private String path;
@@ -65,33 +65,33 @@ public class InputPanel extends JPanel implements ActionListener{
 	private View frame;
 	public Desktop d;
 
-	
 	public InputPanel(final IFile input, GenerateXML xml, View frame) {
 		this.xml = xml;
 		this.frame = frame;
-		
+
 		this.setLayout(new BorderLayout());
-		
+
 		open = new JButton("Open");
-		generate = new JButton("Version: " + xml.getConfiguration().getVersion().getVersion());
+		generate = new JButton("Version: "
+				+ xml.getConfiguration().getVersion().toString());
 		exit = new JButton("Exit");
-		bug = new JButton("Bug/Issue Report");
+		setBugOrDownload(new JButton("Bug/Issue Report"));
 		generate.setEnabled(false);
 
-
-		this.setBorder(new TitledBorder("Generates XML File to Import in ILIAS e-Learning System"));
+		this.setBorder(new TitledBorder(
+				"Generates XML File to Import in ILIAS e-Learning System"));
 		this.add(generate);
 		this.add(open);
-		this.add(bug);
+		this.add(getBugOrDownload());
 		this.add(exit);
 		this.setLayout(new GridLayout(2, 2));
-		
+
 		generate.addActionListener(this);
 		open.addActionListener(this);
 		exit.addActionListener(this);
-		bug.addActionListener(this);
+		getBugOrDownload().addActionListener(this);
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("Open")) {
@@ -99,7 +99,8 @@ public class InputPanel extends JPanel implements ActionListener{
 		}
 		if (e.getActionCommand().equals("Generate XML")) {
 			try {
-				xml.GenerateXMLFile(input, getFileNameWithoutExtension(getFilename())+".xml");
+				xml.GenerateXMLFile(input,
+						getFileNameWithoutExtension(getFilename()) + ".xml");
 				frame.getStatus().setText("XML File has been Generated");
 				frame.getStatus().setForeground(Color.green.darker());
 			} catch (Exception e1) {
@@ -112,24 +113,46 @@ public class InputPanel extends JPanel implements ActionListener{
 			System.exit(0);
 		}
 		if (e.getActionCommand().equals("Bug/Issue Report")) {
-				try {
-					 
-						URI u;
-						d = Desktop.getDesktop();
-						u = new URI("http://code.google.com/p/ilias-userimport/issues/list");
-						d.browse(u); 
-					
-				} catch (URISyntaxException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-					frame.getStatus().setText("ERROR");
-					frame.getStatus().setForeground(Color.red.darker());
-				} catch (IOException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-					frame.getStatus().setText("ERROR");
-					frame.getStatus().setForeground(Color.red.darker());
-				}
+			try {
+
+				URI u;
+				d = Desktop.getDesktop();
+				u = new URI(
+						"http://code.google.com/p/ilias-userimport/issues/list");
+				d.browse(u);
+
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				frame.getStatus().setText("ERROR");
+				frame.getStatus().setForeground(Color.red.darker());
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+				frame.getStatus().setText("ERROR");
+				frame.getStatus().setForeground(Color.red.darker());
+			}
+		}
+		if (e.getActionCommand().equals("DOWNLOAD NOW")) {
+			try {
+
+				URI u;
+				d = Desktop.getDesktop();
+				u = new URI(
+						"http://code.google.com/p/ilias-userimport/downloads/list");
+				d.browse(u);
+
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+				frame.getStatus().setText("ERROR");
+				frame.getStatus().setForeground(Color.red.darker());
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+				frame.getStatus().setText("ERROR");
+				frame.getStatus().setForeground(Color.red.darker());
+			}
 		}
 	}
 
@@ -145,22 +168,24 @@ public class InputPanel extends JPanel implements ActionListener{
 		}
 		return input.substring(0, index);
 	}
-	
+
 	public void Browse() {
 		JFileChooser c = new JFileChooser();
 		c.setMultiSelectionEnabled(false);
 		c.setAcceptAllFileFilterUsed(false);
-		FileNameExtensionFilter filter1 = new FileNameExtensionFilter("Excel 97-2004", "xls");
-		FileNameExtensionFilter filter2 = new FileNameExtensionFilter("CSV", "csv");
+		FileNameExtensionFilter filter1 = new FileNameExtensionFilter(
+				"Excel 97-2004", "xls");
+		FileNameExtensionFilter filter2 = new FileNameExtensionFilter("CSV",
+				"csv");
 		c.setFileFilter(filter1);
 		c.setFileFilter(filter2);
-		
+
 		// Demonstrate "Open" dialog:
 		int rVal = c.showOpenDialog(this);
 		if (rVal == JFileChooser.APPROVE_OPTION) {
-			if(c.getFileFilter() == filter1)
+			if (c.getFileFilter() == filter1)
 				input = new ParseExcel();
-			if(c.getFileFilter() == filter2)
+			if (c.getFileFilter() == filter2)
 				input = new ParseCSV();
 			setFilename(c.getSelectedFile().getName());
 			dir = c.getCurrentDirectory().toString();
@@ -196,7 +221,8 @@ public class InputPanel extends JPanel implements ActionListener{
 	}
 
 	/**
-	 * @param filename the filename to set
+	 * @param filename
+	 *            the filename to set
 	 */
 	public void setFilename(String filename) {
 		this.filename = filename;
@@ -210,9 +236,18 @@ public class InputPanel extends JPanel implements ActionListener{
 	}
 
 	/**
-	 * @param path the path to set
+	 * @param path
+	 *            the path to set
 	 */
 	public void setPath(String path) {
 		this.path = path;
+	}
+
+	public JButton getBugOrDownload() {
+		return bugOrDownload;
+	}
+
+	public void setBugOrDownload(JButton bugOrDownload) {
+		this.bugOrDownload = bugOrDownload;
 	}
 }
