2b331b8f5793c156a25ff162762899503c5ee30b
iFadi <fadi_asbih@yahoo.de>
2014-06-24 11:05:03 +0200

    Refactoring the whole IUI Code.

---
+++ src/de/unihannover/elsa/iui/view/RootLayoutController.java
@@ -0,0 +1,154 @@
+package de.unihannover.elsa.iui.view;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
+import org.apache.commons.io.FilenameUtils;
+import org.controlsfx.dialog.Dialogs;
+
+import de.unihannover.elsa.iui.MainApp;
+
+
+/**
+ * The controller for the root layout. The root layout provides the basic
+ * application layout containing a menu bar and space where other JavaFX
+ * elements can be placed.
+ * 
+ * @author Marco Jakob
+ * @author Fadi Asbih
+ * 
+ */
+public class RootLayoutController {
+
+    // Reference to the main application
+    private MainApp mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    /**
+     * Creates an empty address book.
+     */
+    @FXML
+    private void handleNew() {
+        mainApp.getUserData().clear();
+        mainApp.setUserFilePath(null);
+    }
+
+    /**
+     * Opens a FileChooser to let the user select an address book to load.
+     */
+    @FXML
+    private void handleOpen() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save file dialog
+        File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            mainApp.loadUserDataFromFile(file);
+        }
+    }
+    
+    /**
+     * Opens a FileChooser to let the user select an address book to load.
+     * @throws IOException 
+     */
+    @FXML
+    private void handleImport() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter        
+        FileChooser.ExtensionFilter filter1 = new FileChooser.ExtensionFilter(
+                "Stud.IP CSV file (*.csv)", "*.csv");
+        FileChooser.ExtensionFilter filter2 = new FileChooser.ExtensionFilter(
+        		"Excel 97-2004 (*.xls)", "*.xls");
+        fileChooser.getExtensionFilters().add(filter1);
+        fileChooser.getExtensionFilters().add(filter2);
+		
+        // Show save file dialog
+        File file = fileChooser.showOpenDialog(mainApp.getPrimaryStage());
+
+        if(file != null) {
+            String fileExtension = FilenameUtils.getExtension(file.getPath());
+            if (fileExtension.equals("csv")) {
+                mainApp.parseCSV(file);
+            }
+            if (fileExtension.equals("xls")) {
+                mainApp.parseExcel(file);
+            }
+        }
+    }
+
+    /**
+     * Saves the file to the person file that is currently open. If there is no
+     * open file, the "save as" dialog is shown.
+     */
+    @FXML
+    private void handleSave() {
+        File personFile = mainApp.getUserFilePath();
+        if (personFile != null) {
+            mainApp.saveUserDataToFile(personFile);
+        } else {
+            handleSaveAs();
+        }
+    }
+
+    /**
+     * Opens a FileChooser to let the user select a file to save to.
+     */
+    @FXML
+    private void handleSaveAs() {
+        FileChooser fileChooser = new FileChooser();
+
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+                "XML files (*.xml)", "*.xml");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save file dialog
+        File file = fileChooser.showSaveDialog(mainApp.getPrimaryStage());
+
+        if (file != null) {
+            // Make sure it has the correct extension
+            if (!file.getPath().endsWith(".xml")) {
+                file = new File(file.getPath() + ".xml");
+            }
+            mainApp.saveUserDataToFile(file);
+        }
+    }
+
+    /**
+     * Opens an about dialog.
+     */
+    @FXML
+    private void handleAbout() {
+        Dialogs.create()
+            .title("ILIAS User Import")
+            .masthead("About")
+            .message("\nMost of Code Originally from Marco Jakob\nWebsite: http://code.makery.ch\n\nAuthor: Fadi Asbih\nVersion: 2.0 beta")
+            .showInformation();
+    }
+
+    /**
+     * Closes the application.
+     */
+    @FXML
+    private void handleExit() {
+        System.exit(0);
+    }
+}
